2880f7710039bd72091d759fa0b5b0e228d09f5d
Pedro Trindade <trindade.pedro@gmail.com>
2017-07-27 12:58:58 +0100

    add composer config

---
+++ lib/symfony/lib/task/generator/skeleton/project/config/ProjectConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+require_once dirname(__FILE__) . '/../lib/vendor/autoload.php';
+require_once ##SYMFONY_CORE_AUTOLOAD##;
+    sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+
+    public function setup()
+    {
+
+    }
+}
